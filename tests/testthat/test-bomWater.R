c4cea15ba8d64a7096c2bc13011760fa172c968b
Callum Savage <callum.s.savage@gmail.com>
2023-05-24 19:02:22 +1000

    Test that large requests cause an error

--- tests/testthat/test-bomWater.R
+++ tests/testthat/test-bomWater.R
@@ -17,6 +17,19 @@ test_that("I can make requests to BoM", {
   expect_equal(nrow(r), 1)
 })
 
+test_that("Large requests cause an error", {
+  # Request with > 250000 results
+  params <- list(
+    request = "getTimeseriesValues",
+    ts_id = "1571010",
+    from = "1900-01-01T00:00:00+10:00",
+    to = "2020-01-31T00:00:00+10:00",
+    returnfields = "Timestamp,Value,Quality Code,Interpolation Type"
+  )
+  expect_error(make_bom_request(params))
+  # TODO add error message checking
+})
+
 test_that("I can get a station list", {
   r <- get_station_list(
     parameter_type = "Rainfall",
@@ -30,7 +43,10 @@ test_that("I can get a station list", {
   expect_equal(r$station_id, 13643)
   expect_equal(r$station_latitude,-35.64947222)
   expect_equal(r$station_longitude, 148.83144444)
+})
 
+
+test_that("I can get a list of multiple stations", {
   r <- get_station_list(
     parameter_type = "Rainfall",
     station_number = c("570946", "410730")
@@ -77,6 +93,8 @@ test_that("No results for parameter type and ID mistmatch", {
   )
 
   expect_equal(nrow(r), 0)
+  expect_equal(ncol(r), 7)
+  expect_equal(class(r), c("tbl_df", "tbl", "data.frame"))
 })
 
 test_that("get timeseries puts it all together", {
